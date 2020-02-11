e67f78c828415dbc2035052ce57eb1a50b377b01
anupsdesign <36090259+anupsdesign@users.noreply.github.com>
2020-02-11 12:37:08 -0500

    Add files via upload

--- script/scripts.js
+++ script/scripts.js
@@ -32,12 +32,14 @@ jQuery(function ($) {
     $.fn.hScroll = function (amount) {
         amount = amount || 120;
         $(this).bind("DOMMouseScroll mousewheel", function (event) {
+            if ($(window).width() > 900) {
             var oEvent = event.originalEvent, 
                 direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta, 
                 position = $(this).scrollLeft();
             position += direction > 0 ? -amount : amount;
             $(this).scrollLeft(position);
             event.preventDefault();
+            }
         })
     };
 });
